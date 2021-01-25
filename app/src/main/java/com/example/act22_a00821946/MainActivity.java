f47919f7aba4bc91a1e12dda1e2c7416681d97bb
Carlos <carlosger_64@hotmail.com>
2021-01-25 11:39:03 -0600

    Nuevo commit

---
+++ app/src/main/java/com/example/act22_a00821946/MainActivity.java
@@ -0,0 +1,17 @@
+// Actividad 2.2
+// Carlos Gerardo Herrera Cortina - A00821946
+
+package com.example.act22_a00821946;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
